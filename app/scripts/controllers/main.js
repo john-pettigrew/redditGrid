adb3c1b225dfe9082183cca35c06ffb350082df6
John Pettigrew <john.pettigrew404@gmail.com>
2014-10-18 01:27:26 -0500

    Fix regex.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,20 +9,26 @@
  */
 angular.module('GridApp')
   .controller('MainCtrl', function ($scope, $http, $timeout) {
+    var endName = '';
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $scope.loadImages = function(){
-      $scope.images = [];
-      $http.get('http://www.reddit.com/r/'+$scope.query+'.json?limit=75').
+      $scope.posts = [];
+      $http.get('http://www.reddit.com/r/'+$scope.query+'.json?limit=75'+endName).
       success(function(data){
+        console.log(data.data.children[data.data.children.length-1].data.name);
+        endName = '&after='+data.data.children[data.data.children.length-1].data.name;
+        console.log(endName);
         for(var element in data.data.children){
           var url = data.data.children[element].data.url;
           //check if the website is imgur
-          if(url.indexOf('i.imgur.com/') > -1){
-            $scope.images.push(url);
+          if(url.match(/imgur.com\/[a-z\d]{6}[^_]/i) !== null){
+            console.log(url.match(/imgur.com\/[a-z\d]{7}[^_]/i));
+            //console.log(data.data.children[element].data);
+            $scope.posts.push({url: url, link: 'http://reddit.com'+data.data.children[element].data.permalink, title: data.data.children[element].data.title});
           }
         }
         $timeout(function(){
@@ -45,6 +51,13 @@ angular.module('GridApp')
       .error(function(){
         console.log('error loading images.');
       });
+    } // end loadImages
+    $scope.details = function(image){
+      console.log(image);
+      $scope.modalLink = image.post.link;
+      $scope.modalTitle = image.post.title;
+      $scope.modalUrl = image.post.url;
+      $('#imageModal').modal();
     }
     //select correct tab
     $('.about-tab').removeClass('active');
